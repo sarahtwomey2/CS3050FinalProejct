9a8be07e724773c481f6777bb4ea8bacc904e30d
sarahtwomey2 <31322452+sarahtwomey2@users.noreply.github.com>
2017-12-06 21:48:54 -0600

    something is seriously wrong here

--- main.c
+++ main.c
@@ -9,53 +9,43 @@
 #include <stdio.h>
 #include "parsing.h"
 
-void solveMaze(int x, int y);
-int recursiveSolve(int x, int y);
+//void solveMaze(int x, int y);
+int recursiveSolve(int x, int y, char** content.memory);
 
-char maze[6][6] = {'S',' ',' ','#','#','#',
-    ' ',' ',' ',' ',' ','#',
-    ' ','#','#','#',' ','#',
-    ' ',' ','#','#','E',' ',
-    '#','#','#','#','#','#',
-    '#',' ','#','#','#','#'};
 
-int wasHere[6][6];
-int correctPath[6][6];
-//int startX =0;
-//int startY =0;
-//int endX =3;
-//int endY =4;
-int width = 6;
-int height = 6;
+
+int wasHere[200][500];
+int correctPath[200][500];
+//int width = 6;
+//int height = 6;
 
 int main(int argc, char * argv[]) {
-    printf("original: \n");
-    
+   
     fileData content = readFile(argc, *(argv+1));
+    //char maze[content.rowSize[0]][content.lineSize[0]] = content.memory;
     
+    for(int row=0; row< *content.rowSize; row++) {
+        for(int col=0; col < *content.lineSize[row]; col++) {
+            wasHere[row][col] = 0;
+            correctPath[row][col] = 0;
+        }
+    }
     
+    int b = recursiveSolve(content.start[0][0], content.start[0][1], content.memory);
     
-    //    for (int row = 0; row < *content.rowSize; row++)
-    //        // Sets boolean Arrays to default values
-    //        for (int col = 0; col < 6; col++){
-    //            wasHere[row][col] = 0;
-    //            correctPath[row][col] = 0;
-    //        }
-    
-    int b = recursiveSolve(content.start[0][0], content.start[0][1]);
     if(b == 1) {
         printf("\nSuccess");
     } else {
         printf("\nCould not find way out of maze");
     }
-    
+    /*
     printf("\n\nprinting new maze:\n");
     for(int i=0; i<6; i++) {
         for (int j=0; j<6; j++) {
             printf(" %c", maze[i][j]);
         }
         printf("\n");
-    }
+    }*/
     
     // Will leave you with a boolean array (correctPath)
     // with the path indicated by true values.
@@ -66,7 +56,7 @@ int main(int argc, char * argv[]) {
 
 
 
-int recursiveSolve(int x, int y) {
+int recursiveSolve(int x, int y, char** maze) {
     //0 = false, 1 = true
     
     if (maze[x][y] == 'E') return 1; // If you reached the end
@@ -76,7 +66,7 @@ int recursiveSolve(int x, int y) {
     if (x != 0) // Checks if not on left edge
         if (recursiveSolve(x-1, y)) { // Recalls method one to the left
             correctPath[x][y] = 1; // Sets that path value to true;
-            maze[x][y] = '+'; //changing value from . to +
+            content.memory = '+'; //changing value from . to +
             return 1;
         }
     if (x != width - 1) // Checks if not on right edge
@@ -92,12 +82,12 @@ int recursiveSolve(int x, int y) {
             return 1;
         }
     if (y != height - 1) // Checks if not on bottom edge
-        if (recursiveSolve(x, y+1)) { // Recalls method one down
+        if (recursiveSolve(x, y+1)) {  //Recalls method one down
             correctPath[x][y] = 1;
             maze[x][y] = '+';
             return 1;
         }
-    maze[x][y] = 'x';
+    
+    //maze[x][y] = 'x';
     return 0;
 }
-
